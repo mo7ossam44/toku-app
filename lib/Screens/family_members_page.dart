73c41701e4436524254060fd621becdf1c980681
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-17 02:20:30 +0300

    add family members page

---
+++ lib/Screens/family_members_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FamilyMembersPage extends StatelessWidget {
+  const FamilyMembersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Toku',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.brown,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+    );
+  }
+}
