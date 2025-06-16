c571405e1ce6d67d518c4c3b4d1d9142295aa9bf
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-17 01:41:45 +0300

    Add main items widget for home page

--- lib/Components/main_items.dart
+++ lib/Components/main_items.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
 
 class MainItems extends StatelessWidget {
-  const MainItems({super.key, required this.text, required this.color});
+  const MainItems({super.key, required this.text, required this.color, required this.ontap});
 
   final String text;
   final Color color;
+  final VoidCallback ontap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(left: 10),
-        alignment: Alignment.centerLeft,
-        color: color,
-        width: double.infinity,
-        height: 70,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+          padding: EdgeInsets.only(left: 10),
+          alignment: Alignment.centerLeft,
+          color: color,
+          width: double.infinity,
+          height: 70,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
           ),
         ),
-      );
+    );
   }
 }
