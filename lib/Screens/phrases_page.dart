2a86c9741b3816ac6efafe207000c1cbde13e32f
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-18 20:07:14 +0300

    finish phrases page

--- lib/Screens/phrases_page.dart
+++ lib/Screens/phrases_page.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:tuku/Components/phrases_item.dart';
+import 'package:tuku/Models/Item_model.dart';
 
 class PhrasesPage extends StatelessWidget {
-  const PhrasesPage({super.key});
+  PhrasesPage({super.key});
+
+  List<ItemModel> phrasesList = [
+    ItemModel(
+      jpName: 'kodoku suru koto o wesurenaide kudsi',
+      enName: "don't forgot to subscribe",
+      sound: 'sound',
+    ),
+    ItemModel(
+      jpName: 'kodoku suru koto o wesurenaide kudsi',
+      enName: "don't forgot to subscribe",
+      sound: 'sound',
+    ),
+    ItemModel(
+      jpName: 'kodoku suru koto o wesurenaide kudsi',
+      enName: "don't forgot to subscribe",
+      sound: 'sound',
+    ),
+    ItemModel(
+      jpName: 'kodoku suru koto o wesurenaide kudsi',
+      enName: "don't forgot to subscribe",
+      sound: 'sound',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +38,12 @@ class PhrasesPage extends StatelessWidget {
         ),
         backgroundColor: Colors.brown,
         iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: ListView.builder(
+        itemCount: phrasesList.length,
+        itemBuilder: (context, index) {
+          return PharsesItem(itemModel: phrasesList[index], color: Colors.blue);
+        },
       ),
     );
   }
