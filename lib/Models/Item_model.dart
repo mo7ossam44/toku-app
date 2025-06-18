188df714151277c770e5f0b127b4bcbe2fc88634
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-18 20:06:57 +0300

    make image in required

--- lib/Models/Item_model.dart
+++ lib/Models/Item_model.dart
@@ -1,11 +1,11 @@
 class ItemModel {
-  final String image;
+  final String? image;
   final String enName;
   final String jpName;
   final String sound;
 
   ItemModel({
-    required this.image,
+    this.image,
     required this.jpName,
     required this.enName,
     required this.sound,
